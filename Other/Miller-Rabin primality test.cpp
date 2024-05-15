9098dd9450e808e29541c9c019687451e8a9cb48
Mohammadreza Mohammadzadeh Asl <mohammadrezamhmz@gmail.com>
2024-05-15 17:50:03 +0330

    Update Miller-Rabin Implementation

--- Other/Miller-Rabin primality test.cpp
+++ Other/Miller-Rabin primality test.cpp
@@ -1,50 +1,46 @@
-bool miillerTest(int d, int n) 
-{ 
-    // Pick a random number in [2..n-2] 
-    // Corner cases make sure that n > 4 
-    int a = 2 + rand() % (n - 4); 
-  
-    // Compute a^d % n 
-    int x = power(a, d, n); 
-  
-    if (x == 1  || x == n-1) 
-       return true; 
-  
-    // Keep squaring x while one of the following doesn't 
-    // happen 
-    // (i)   d does not reach n-1 
-    // (ii)  (x^2) % n is not 1 
-    // (iii) (x^2) % n is not n-1 
-    while (d != n-1) 
-    { 
-        x = (x * x) % n; 
-        d *= 2; 
-  
-        if (x == 1)      return false; 
-        if (x == n-1)    return true; 
-    } 
-  
-    // Return composite 
-    return false; 
-} 
-  
-// k is an input parameter that determines 
-// accuracy level. Higher value of k indicates more accuracy. 
-bool isPrime(int n, int k) 
-{ 
-    // Corner cases 
-    if (n <= 1 || n == 4)  return false; 
-    if (n <= 3) return true; 
-  
-    // Find r such that n = 2^d * r + 1 for some r >= 1 
-    int d = n - 1; 
-    while (d % 2 == 0) 
-        d /= 2; 
-  
-    // Iterate given nber of 'k' times 
-    for (int i = 0; i < k; i++) 
-         if (!miillerTest(d, n)) 
-              return false; 
-  
-    return true; 
-} 
+using u64 = uint64_t;
+using u128 = __uint128_t;
+
+u64 binpower(u64 base, u64 e, u64 mod) {
+    u64 result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (u128)result * base % mod;
+        base = (u128)base * base % mod;
+        e >>= 1;
+    }
+    return result;
+}
+
+bool check_composite(u64 n, u64 a, u64 d, int s) {
+    u64 x = binpower(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (u128)x * x % n;
+        if (x == n - 1)
+            return false;
+    }
+    return true;
+};
+
+bool millerRabin(u64 n) { // returns true if n is prime, else returns false.
+    if (n < 2)
+        return false;
+
+    int r = 0;
+    u64 d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
+        r++;
+    }
+
+    for (u64 a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+        if (n == a)
+            return true;
+        if (check_composite(n, a, d, r))
+            return false;
+    }
+    return true;
+}
